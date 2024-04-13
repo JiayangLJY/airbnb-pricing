6cc361160a5872213a29a5d359c7edd6bae0cd23
jiayangliu <liujiayang_ljy@163.com>
2024-04-13 12:12:20 -0700

    format prediction page

--- pages/data_exploration.py
+++ pages/data_exploration.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from joblib import load
 import plotly.express as px
 import geopandas as gpd
 
--- prediction.py
+++ prediction.py
@@ -1,21 +1,16 @@
 import streamlit as st
 import pandas as pd
-# import joblib
 from joblib import load
 
 ############ Const & Config ############
-# MODEL_PATH = './model/model.joblib'
 MODEL_PATH = './model/best_gb_params_rs.joblib'
 MODEL_PATH2 = '../../modeling/bagging_stacked_model.joblib'
-# DATA_PATH = './data/default_input.csv'
 DATA_PATH = './data/default_input_opt.csv'
 NBR = ('Adams', 'Alki', 'Arbor Heights', 'Atlantic', 'Belltown', 'Bitter Lake', 'Briarcliff', 'Brighton', 'Broadview', 'Broadway', 'Bryant', 'Cedar Park', 'Central Business District', 'Columbia City', 'Crown Hill', 'Dunlap', 'East Queen Anne', 'Eastlake', 'Fairmount Park', 'Fauntleroy', 'First Hill', 'Fremont', 'Gatewood', 'Genesee', 'Georgetown', 'Green Lake', 'Greenwood', 'Haller Lake', 'Harbor Island', 'Harrison/Denny-Blaine', 'High Point', 'Highland Park', 'Holly Park', 'Industrial District', 'Interbay', 'International District', 'Laurelhurst', 'Lawton Park', 'Leschi', 'Lower Queen Anne', 'Loyal Heights', 'Madison Park', 'Madrona', 'Mann', 'Maple Leaf', 'Matthews Beach', 'Meadowbrook', 'Mid-Beacon Hill', 'Minor', 'Montlake', 'Mount Baker', 'North Admiral', 'North Beach/Blue Ridge', 'North Beacon Hill', 'North College Park', 'North Delridge', 'North Queen Anne', 'Olympic Hills', 'Phinney Ridge', 'Pike-Market', 'Pinehurst', 'Pioneer Square', 'Portage Bay', 'Rainier Beach', 'Rainier View', 'Ravenna', 'Riverview', 'Roosevelt', 'Roxhill', 'Seaview', 'Seward Park', 'South Beacon Hill', 'South Delridge', 'South Lake Union', 'South Park', 'Southeast Magnolia', 'Stevens', 'Sunset Hill', 'University District', 'Victory Heights', 'View Ridge', 'Wallingford', 'Wedgwood', 'West Queen Anne', 'West Woodland', 'Westlake', 'Whittier Heights', 'Windermere', 'Yesler Terrace')
 P_TYPE = ('Entire guesthouse', 'Private room in rental unit', 'Entire home', 'Entire guest suite', 'Entire rental unit', 'Private room in home', 'Entire bungalow', 'Private room in cottage', 'Entire condo', 'Private room', 'Private room in bungalow', 'Entire townhouse', 'Private room in condo', 'Entire cottage', 'Private room in townhouse', 'Private room in guest suite', 'Boat', 'Private room in boat', 'Entire loft', 'Shared room', 'Entire vacation home', 'Private room in treehouse', 'Entire villa', 'Entire cabin', 'Entire place', 'Tent', 'Camper/RV', 'Tiny home', 'Houseboat', 'Entire serviced apartment', 'Shared room in rental unit', 'Private room in hostel', 'Room in boutique hotel', 'Shared room in home', 'Private room in loft', 'Shared room in loft', 'Room in hotel', 'Private room in villa', 'Shared room in townhouse', 'Private room in tiny home', 'Shared room in hostel', 'Private room in bed and breakfast', 'Casa particular', 'Room in aparthotel', 'Shared room in bed and breakfast', 'Farm stay', 'Shared room in condo', 'Bus', 'Private room in casa particular', 'Private room in guesthouse', 'Private room in serviced apartment')
 RT_PRE = 'room_type_'
-# BT_PRE = 'bath_type_'
 BT_PRE = 'bathroom_type_'
 PT_PRE = 'property_type_'
-# NBR_PRE = 'neighbourhood_'
 NBR_PRE = 'neighbourhood_cleansed_'
 
 
@@ -31,7 +26,7 @@ st.sidebar.markdown("Predict you ideal Airbnb rental price using our regression 
 @st.cache_resource
 def load_model(no=1):
     path = MODEL_PATH if no == 1 else MODEL_PATH2
-    return joblib.load(path)
+    return load(path)
 
 # load default user input (as a DataFrame)
 @st.cache_data
@@ -83,28 +78,13 @@ with bathroom_col2:
 pt_input = st.selectbox("Poperty Type", P_TYPE, key="selectbox_poperty")
 
 
-############ Review rating information ############
-# rating_input = st.slider('Rating Score', 0.0, 5.0, 0.1)
-
-
-############ Wrap the input data and Predict price with the model ############
-# def rt_map(rt_input: str) -> str:
-#     rt = 'shared'
-#     if rt_input == 'Entire home/apt':
-#         rt = 'entire'
-#     elif rt_input == 'Private room':
-#         rt = 'private'
-#     return RT_PRE+rt
-
 if st.button("Predict", type='primary'):
     model = load_model(1)
     df_input = default_input()
 
     st.divider()
 
-    # try:
     # categorical cols
-    # df_input.at[0, rt_map(rt_input)] = True
     df_input.at[0, RT_PRE+rt_input] = True
     df_input.at[0, BT_PRE+(bt_input.lower())] = True
     df_input.at[0, NBR_PRE+nbr_input] = True
@@ -115,7 +95,6 @@ if st.button("Predict", type='primary'):
     df_input.at[0, 'bedrooms'] = bedroom_input
     df_input.at[0, 'beds'] = bed_input
     df_input.at[0, 'bathroom_count'] = bathroom_input
-    # df_input.at[0, 'rating'] = rating_input
 
     result = model.predict(df_input)[0]
 
@@ -130,6 +109,3 @@ if st.button("Predict", type='primary'):
     res_col21.metric('Area', f'{nbr_input}')
     res_col22.metric('Bed', f'{bed_input}')
     res_col23.metric('Price', f'${round(result, 2)}')
-    
-    # except Exception as e:
-    #     st.error('Something went wrong, try again please', icon="🚨")
